e527e341e42e01cb6c787c37f17c2693642abe4b
Amodeo Lorenzo <lorenzo@solutions.io>
2022-06-16 12:34:24 +0200

    save point

--- src/App.js
+++ src/App.js
@@ -10,13 +10,16 @@ import Map from './components/Map/Map'
 const App = () => {
   return (
     <>
-      <CssBaseline/>
-      <Header/>
-      <Grid container spacing={3} style={{width: '10%'}}>
-
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
       </Grid>
-      <List/>
-      <Map/>
     </>
   )
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'
+import { EventNote, LocationOnOutlined } from '@material-ui/icons'
+import Rating from '@material-ui/lab'
+
+import useStyles from './styles'
 
 const Map = () => {
+  const classes = useStyles()
+  const isMobile = useMediaQuery('(min-width: 600px)')
+
+  const coordinates = { lat: 0, lng: 0 }
+
   return (
-    <h1>Map</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyDcp-e7g7o_rTC9PES0nz0ZoMq5r3E1Diw' }}
+
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50,50,50,50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >
+
+      </GoogleMapReact>
+    </div>
   )
 }
 
