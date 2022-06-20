0d0d9e11013e7e57c1be2f3392ec880ef266ab47
Amodeo Lorenzo <lorenzo@solutions.io>
2022-06-20 12:24:56 +0200

    pointer in the map

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ const App = () => {
   const [places, setPlaces] = useState([])
 
   const [coordinates, setCoordinates] = useState({})
-  const [bounds, setBounds] = useState({ sw: 0, ne: 0})
+  const [bounds, setBounds] = useState({ sw: 0, ne: 0 })
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
-          setCoordinates({ lat: latitude, lng: longitude })
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoordinates({ lat: latitude, lng: longitude })
     })
-  }, [] )
+  }, [])
 
   useEffect(() => {
     console.log(bounds);
@@ -39,10 +39,15 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List places={places}/>
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates}/>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={places}
+          />
         </Grid>
       </Grid>
     </>
